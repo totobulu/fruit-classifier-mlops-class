83acbf03eaa8966fead9be6774fc68a5f61b018d
totobulu <153515362+totobulu@users.noreply.github.com>
2024-12-01 14:00:25 +0000

    load model

--- app/model.py
+++ app/model.py
@@ -1,12 +1,17 @@
 import os
 import wandb
 from loadotenv import load_env
+from pathlib import Path
+import torch
+from torchvision.models import resnet18, ResNet
+from torch import nn
+from torchvision import transforms
 
 load_env(file_loc='/workspaces/fruit-classifier-mlops-class/.env')
 wandb_api_key = os.getenv('WANDB_API_KEY')
-print(wandb_api_key)
 
 MODELS_DIR = 'models'
+MODEL_FILE_NAME = 'best_model.pth'
 
 os.makedirs(MODELS_DIR, exist_ok=True)
 
@@ -20,6 +25,34 @@ def download_artifact():
 
     artifact_path = f"{wandb_org}/{wandb_project}/{wandb_model_name}:{wandb_model_version}"
 
+    wandb.login()
+
+    artifact = wandb.Api().artifact(artifact_path, type='model')
+    artifact.download(root=MODELS_DIR)
+
     print(artifact_path)
 
-download_artifact()
+def get_raw_model() -> ResNet:
+    """" Here we create a model with the same architecture as the one that we have on Kaggle, but without any weights"""
+    architecture = resnet18(weights=None)
+    # Change the model architecture to the one that we are actually using    
+    architecture.fc = nn.Sequential(
+    nn.Linear(512, 512),
+    nn.ReLU(),
+    nn.Linear(512, 6)
+    )
+    return architecture
+
+def load_model() -> ResNet:
+    """" This returns the model with its wandb weights"""
+    download_artifact()
+    model = get_raw_model()
+    model_state_dict_path = Path(MODELS_DIR) / MODEL_FILE_NAME
+    model_state_dict = torch.load(model_state_dict_path, map_location='cpu')
+    #assign the trained model weights to model
+    model.load_state_dict(model_state_dict, strict=True)
+    #turn off Batchnorm and dropout
+    model.eval()
+    return model
+
+print(load_model())
